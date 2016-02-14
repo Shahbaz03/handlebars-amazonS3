0ffe59861e6a245fc749bb5531071bb8e6f70959
shahbaz <shahbazalam03@gmail.com>
2016-02-14 22:42:01 +0530

    Code Commit

--- src/main/java/com/services/App.java
+++ src/main/java/com/services/App.java
@@ -27,7 +27,7 @@ import com.github.jknack.handlebars.Template;
  */
 public class App {
 	/**
-	 * intialize the AWS credentials with the access-key and the secret-key
+	 * Intialize the AWS credentials with the access-key and the secret-key
 	 */
 	private static  AWSCredentials AWS_CREDENTIALS = new BasicAWSCredentials("AWS_ACCESS_KEY",
 		      "AWS_SECRET_KEY");
